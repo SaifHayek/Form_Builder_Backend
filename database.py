b63f5dd2428e138914a5d2ec2f083020dd78a52a
SaifHayek <90109558+SaifHayek@users.noreply.github.com>
2025-01-12 22:05:45 +0200

    remove url file

--- database.py
+++ database.py
@@ -4,7 +4,7 @@ from typing import Annotated
 import os
 
 
-SQLALCHEMY_DATABASE_URL = os.getenv("DATABASE_URL", "postgresql://form_builder_fp8b_user:bskewpGy4ICsWjTZN5nNdpvp461pypeD@dpg-cu15q3rqf0us73d7kdag-a.oregon-postgres.render.com/form_builder_fp8b")
+SQLALCHEMY_DATABASE_URL = os.getenv("DATABASE_URL")
 
 # SQLALCHEMY_DATABASE_URL = os.getenv("DATABASE_URL", "postgresql://postgres:password@localhost:5432/form_builder")
 connect_args = {"check_same_thread": False}
